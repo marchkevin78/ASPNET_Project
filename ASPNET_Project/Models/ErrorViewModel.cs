864ba2b7b05a7659d38902c183eb27407b65737e
Kevin March <marchkevin78@gmail.com>
2021-01-05 18:52:07 -0500

    first commit

---
+++ ASPNET_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNET_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
